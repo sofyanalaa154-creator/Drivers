e63eb470c3a21bf526f91771564d601cc287d797
SofyanAlaa3 <sofyanalaa154@gmail.com>
2026-09-14 22:36:45 +0300

    Update Interrupt Interface.h

--- Interrupt Interface.h
+++ Interrupt Interface.h
@@ -1,28 +1,27 @@
-#ifndef INTERRUPT_INTERFACE_H
-#define INTERRUPT_INTERFACE_H
+#ifndef EXTI_INTERFACE_H
+#define EXTI_INTERFACE_H
+
 #include "STD_TYPES"
 
-#define EXTI_INT0   0
-#define EXTI_INT1   1
+#define EXTI_INT0        0
+#define EXTI_INT1        1
 
-#define LOW_LEVEL       0
-#define ANY_CHANGE      1
-#define FALLING_EDGE    2
-#define RISING_EDGE     3
+#define LOW_LEVEL        0
+#define ANY_CHANGE       1
+#define FALLING_EDGE     2
+#define RISING_EDGE      3
 
+#define ISC00            0
+#define ISC01            1
+#define ISC10            2
+#define ISC11            3
 
-#define ISC00   0
-#define ISC01   1
-#define ISC10   2
-#define ISC11   3
-
-
+#define INT0             0
+#define INT1             1
 
 void EXTI_Init(u8 Copy_u8INT, u8 Copy_u8Sense);
-
 void EXTI_Enable(u8 Copy_u8INT);
 void EXTI_Disable(u8 Copy_u8INT);
-
 void EXTI_SetCallBack(u8 Copy_u8INT, void (*ptr)(void));
 
 #endif
